7c0b2795f4bdc225af1280d6961f89546137113c
William Rook <59263638+drhieu@users.noreply.github.com>
2020-10-21 18:03:05 -0500

    Update install.sh

--- opscripts/install.sh
+++ opscripts/install.sh
@@ -6,3 +6,6 @@ sudo cp $DIR/env.sh /etc/roverrobotics/env.sh
 sudo cp $DIR/roscore.service /etc/systemd/system/roscore.service
 sudo cp $DIR/roverrobotics.service /etc/systemd/system/roverrobotics.service
 sudo cp $DIR/roverrobotics /usr/sbin/roverrobotics
+sudo systemctl enable roverrobotics.service
+sudo systemctl enable roscore.service
+sudo chmod +x /usr/sbin/roverrobotics
